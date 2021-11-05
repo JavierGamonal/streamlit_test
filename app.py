7a3a86ceca0a3acf6d83f0f2ed319b12d6a175a1
Javier Gamonal Lara <43260034+JavierGamonal@users.noreply.github.com>
2021-11-05 20:04:31 -0300

    ad

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import altair as altair
 st.title('DataFrame Demo')
 st.sidebar.selectbox('Choose a demo', ['DataFrame Demo','Other'])
 check=st.sidebar.checkbox('Show code')
-df = pd.read_csv('https://github.com/napoles-uach/Pycon_cl_taller/blob/main/meteorite-landings.csv')
+file_name = "https://raw.githubusercontent.com/napoles-uach/Pycon_cl_taller/main/appDemo/UNdata_Export_20211101_202548548.csv"
+df = pd.read_csv(file_name)
 
 st.write(df)
 
