913b5665842f23f1020d2800519b115c1c39c25a
Javier Gamonal Lara <43260034+JavierGamonal@users.noreply.github.com>
2021-11-05 20:28:53 -0300

    as

--- appmap.py
+++ appmap.py
@@ -23,5 +23,5 @@ cols=list(df.columns) # lista con nombres de columnas en el csv
 
 col1,col2,col3 = st.columns([5,1,5])
 with col1.expander('widgets'):
-    year_range = st.slider('year range',year_min,year_max,[100,1900],step = 10)
+    year_range = st.slider('year range',year_min,year_max,[1800,1900],step = 10)
     vals=st.multiselect('',cols)
