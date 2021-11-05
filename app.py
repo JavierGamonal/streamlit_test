6fc16edac5b032dd37426a5568955a6ef550536b
Javier Gamonal Lara <43260034+JavierGamonal@users.noreply.github.com>
2021-11-05 19:53:49 -0300

    asd

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ import altair as altair
 
 
 st.title('DataFrame Demo')
-st.sidebar.selectbox('Choose a demo', ['DataFrame Demo'])
+st.sidebar.selectbox('Choose a demo', ['DataFrame Demo','Other'])
+
 
