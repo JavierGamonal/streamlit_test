fe14b23d3e8729f6ce9fb87368691be462a33723
Javier Gamonal Lara <43260034+JavierGamonal@users.noreply.github.com>
2021-11-05 20:03:18 -0300

    asd

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ st.sidebar.selectbox('Choose a demo', ['DataFrame Demo','Other'])
 check=st.sidebar.checkbox('Show code')
 df = pd.read_csv('https://github.com/napoles-uach/Pycon_cl_taller/blob/main/meteorite-landings.csv')
 
+st.write(df)
 
