843a015565ef6f339e7f03f78eabef9b9496d1e3
Javier Gamonal Lara <43260034+JavierGamonal@users.noreply.github.com>
2021-11-05 20:01:20 -0300

    asd

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ import altair as altair
 st.title('DataFrame Demo')
 st.sidebar.selectbox('Choose a demo', ['DataFrame Demo','Other'])
 check=st.sidebar.checkbox('Show code')
+df = pd.read_csv('https://github.com/napoles-uach/Pycon_cl_taller/blob/main/meteorite-landings.csv')
 
 
