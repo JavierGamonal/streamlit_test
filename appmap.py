d0391c316e0e41708e9c0ac421d8d814d027f769
Javier Gamonal Lara <43260034+JavierGamonal@users.noreply.github.com>
2021-11-05 20:21:49 -0300

    asd

--- appmap.py
+++ appmap.py
@@ -4,3 +4,11 @@ st.title('AppMap')
 st.markdown('Esta es una aplicación simple para ayudarte a visualizar mapas')
 st.markdown('**Nota:** Esta aplicación sigue en desarrollo, cualquier bug reportarlo a [@napoles3D](https://twitter.com/napoles3D)')
 
+filepath = 'https://raw.githubusercontent.com/napoles-uach/Pycon_cl_taller/main/meteorite-landings.csv'
+
+#st.cache
+def read_csv(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+df = read_csv(filepath)
