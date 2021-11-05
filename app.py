c3f68d6b6dcb8e1833f2ca6276e69309c429aee9
Javier Gamonal Lara <43260034+JavierGamonal@users.noreply.github.com>
2021-11-05 19:48:36 -0300

    asd

--- app.py
+++ app.py
@@ -1,9 +1,2 @@
 import streamlit as st
-st.title('Mi primer ejemplo :rocket:')
-x=st.slider('select a value')
-st.write(x, 'squared is', x*x)
-st.sidebar.checkbox('Click me')
-
-st.button('Click')
-       
-st.markdown(':+1:')
+!streamlit hello
