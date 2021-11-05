455baba0b76e4a8ca0b99ae33ff4d2b35ac4929e
Javier Gamonal Lara <43260034+JavierGamonal@users.noreply.github.com>
2021-11-05 20:20:15 -0300

    asd

--- appmap.py
+++ appmap.py
@@ -1,1 +1,6 @@
+import streamlit as st 
+import pandas as pd 
+st.title('AppMap')
+st.markdown('Esta es una aplicación simple para ayudarte a visualizar mapas')
+st.markdown('**Nota:** Esta aplicación sigue en desarrollo, cualquier bug reportarlo a [@napoles3D](https://twitter.com/napoles3D)')
 
